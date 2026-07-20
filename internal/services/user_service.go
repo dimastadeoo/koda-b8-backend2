b7dbd825e56e11d86d10a0ad08d6d50ddae3c1fd
dimastadeoo <dimastadeoo@gmail.com>
2026-07-20 14:11:48 +0700

    feat: Implement Login method in UserService for user authentication

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"errors"
+
 	"github.com/dimastadeoo/backend1/internal/models"
 	"github.com/dimastadeoo/backend1/internal/repo"
 	"golang.org/x/crypto/bcrypt"
@@ -31,4 +33,27 @@ func (r *UserService) Register(user *models.RegisterUsers) (models.Users, error)
 
 func (s *UserService) GetAll() []models.Users {
     return s.repo.GetAll()
+}
+
+func (s *UserService) Login(req *models.LoginUser) (*models.Users, error) {
+
+	user, err := s.repo.FindByEmail(req.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	if user == nil {
+		return nil, errors.New("email atau password salah")
+	}
+
+	err = bcrypt.CompareHashAndPassword(
+		[]byte(user.Password),
+		[]byte(req.Password),
+	)
+
+	if err != nil {
+		return nil, errors.New("email atau password salah")
+	}
+
+	return user, nil
 }
