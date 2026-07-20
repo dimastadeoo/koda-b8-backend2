7c33cb02fdd31dc96560bb7eb5e195a632523b8b
dimastadeoo <dimastadeoo@gmail.com>
2026-07-20 17:52:02 +0700

    refactor: Update UserRepo to use pgxpool for database operations and remove in-memory user storage

--- internal/repo/user_repo.go
+++ internal/repo/user_repo.go
@@ -1,50 +1,115 @@
 package repo
 
 import (
-	"errors"
-	"time"
+	"context"
 
 	"github.com/dimastadeoo/backend1/internal/models"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type UserRepo struct {
-	data *[]models.Users
+	data *pgxpool.Pool
 }
 
-func NewUserRepo(data *[]models.Users) *UserRepo {
+func NewUserRepo(data *pgxpool.Pool) *UserRepo {
 	return &UserRepo{data: data}
 }
 
 func (u *UserRepo) Create(create *models.RegisterUsers) (models.Users, error) {
 
-	for _, user := range *u.data {
-		if user.Email == create.Email {
-			return models.Users{}, errors.New("email already exists")
-		}
+	query := `
+			INSERT INTO users(fullname, email, password)
+			VALUES($1, $2, $3)
+			RETURNING id, fullname, email, password, created_at, updated_at;
+	`
+	var user models.Users
+
+	err := u.data.QueryRow(
+		context.Background(), query,
+		create.Fullname,
+		create.Email,
+		create.Password,
+	).Scan(
+		&user.Id,
+		&user.Fullname,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return models.Users{}, err
 	}
-	user := models.Users{
-		Id:        len(*u.data) + 1,
-		Fullname:  create.Fullname,
-		Email:     create.Email,
-		Password:  create.Password,
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
-	*u.data = append(*u.data, user)
 
 	return user, nil
 }
 
-func (r *UserRepo) GetAll() []models.Users {
-    return *r.data
-}
-
-func (r *UserRepo) FindByEmail(email string) (*models.Users, error) {
-	for _, user := range *r.data {
-		if user.Email == email {
-			return &user, nil
-		}
+func (u *UserRepo) GetAll() ([]models.Users, error) {
+    query := `
+			SELECT id, fullname, email, created_at, updated_at
+			FROM users
+	`
+	data, err := u.data.Query(context.Background(), query)
+	if err != nil{
+		return nil, err
 	}
 
-	return nil, nil
+	defer data.Close()
+
+	users := []models.Users{}
+
+	for data.Next(){
+		var user models.Users
+
+		err := data.Scan(
+			&user.Id,
+			&user.Fullname,
+			&user.Email,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, data.Err()
+}
+
+func (u *UserRepo) FindByEmail(email string) (*models.Users, error) {
+	var user models.Users
+
+	query := `
+			SELECT id, fullname, email, password, created_at, updated_at
+			FROM users
+			WHERE email=$1
+	`
+
+	err := u.data.QueryRow(
+		context.Background(),
+		query,
+		email,
+	).Scan(
+		&user.Id,
+		&user.Fullname,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		if err == pgx.ErrNoRows{
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	return &user, nil
 }
