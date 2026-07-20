db0b7e9537461b15df98df0ddf4db689cccc1fc1
dimastadeoo <dimastadeoo@gmail.com>
2026-07-20 13:48:52 +0700

    feat: Add main application entry point with user registration and retrieval endpoints

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/dimastadeoo/backend1/internal/di"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	container, err := di.NewContainer()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := gin.Default()
+
+	r.POST("/users", container.Users().Register)
+	r.GET("/users", container.Users().GetAll)
+
+	r.Run("0.0.0.0:8080")
+}
