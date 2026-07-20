afb631e8f487de3df6c43702927c15a623cd17dc
dimastadeoo <dimastadeoo@gmail.com>
2026-07-20 13:11:42 +0700

    feat: Implement user repository with create functionality

---
+++ internal/repo/user_repo.go
@@ -0,0 +1,37 @@
+package repo
+
+import (
+	"errors"
+	"time"
+
+	"github.com/dimastadeoo/backend1/internal/models"
+)
+
+type UserRepo struct{
+	data *[]models.Users
+}
+
+func NewUserService (data *[]models.Users) *UserRepo{
+	return &UserRepo{data: data}
+}
+
+func (u *UserRepo) Create(create *models.RegisterUsers) (models.Users, error){
+
+	for _, user := range *u.data {
+		if user.Email == create.Email {
+			return models.Users{}, errors.New("email already exists")
+		}
+	}
+	user := models.Users{
+		Id: len(*u.data) + 1,
+        Fullname: create.Fullname,
+        Email:    create.Email,
+        Password: create.Password,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+    }
+	*u.data = append(*u.data, user)
+
+
+	return user, nil 
+}
