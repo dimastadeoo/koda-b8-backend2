6eb6f609424c36d62f0f4ce1d1cda139598dc39f
dimastadeoo <dimastadeoo@gmail.com>
2026-07-20 13:12:05 +0700

    feat: Implement UserHandler with registration endpoint

---
+++ internal/handler/user_handler.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/dimastadeoo/backend1/internal/lib"
+	"github.com/dimastadeoo/backend1/internal/models"
+	"github.com/dimastadeoo/backend1/internal/services"
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	svc *services.UserService
+}
+
+func NewHandlerUser(svc *services.UserService) *UserHandler {
+	return &UserHandler{svc: svc}
+}
+
+func (h *UserHandler) Register(ctx *gin.Context) {
+	fullname := ctx.PostForm("name")
+	email := ctx.PostForm("email")
+	password := ctx.PostForm("password")
+
+	newUser, err := h.svc.Register(&models.RegisterUsers{
+		Fullname: fullname,
+		Email:    email,
+		Password: password,
+	})
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, lib.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+	} else {
+		ctx.JSON(http.StatusOK, lib.Response{
+			Success: true,
+			Message: fmt.Sprintf("user %s created", newUser.Fullname),
+		})
+	}
+
+}
