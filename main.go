f0503ac43cd7f27d362dcf39feadd3741fb2cb5b
dimastadeoo <dimastadeoo@gmail.com>
2026-07-21 16:03:55 +0700

    feat: Load environment variables and organize routes in main.go for improved structure

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/dimastadeoo/backend1/internal/di"
+	"github.com/dimastadeoo/backend1/internal/middlewares"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 	container, err := di.NewContainer()
 	if err != nil {
 		log.Fatal(err)
@@ -15,10 +19,25 @@ func main() {
 	user := container.Users()
 
 	r := gin.Default()
+	r.Use(middlewares.Cors())
 
-	r.POST("/users", user.Register)
-	r.GET("/users", user.GetAll)
-	r.POST("/login", user.Login)
+	{
+		auth := r.Group("/auth")
+		auth.POST("/register", user.Register)
+		auth.POST("/login", user.Login)
+	}
 
-	r.Run("0.0.0.0:8080")
+	{
+		users := r.Group("/users")
+		users.Use(middlewares.Auth())
+		users.GET("", user.GetAll)
+		users.POST("", user.Register)
+		users.GET("/:id", user.FindById)
+		users.PATCH("/:id", user.Update)
+		users.DELETE("/:id", user.Delete)
+
+	}
+
+	getPort := os.Getenv("PORT")
+	r.Run("0.0.0.0:" + getPort)
 }
